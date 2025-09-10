9b69b478daf24790b1be73c4618e4d0434721d96
SempaiDarcy <mr_darcy_1@mail.ru>
2025-09-10 18:32:53 +0500

    add vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {productsRouter} from "./routers/products-router";
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
-app.get('/',(req,res)=>{
+
+const parserMiddleware = express.json()
+app.use(parserMiddleware)
+
+app.get('/',(req:Request, res:Response)=>{
     res.status(200).send('Hello World')
 })
+
+app.use('/products', productsRouter)
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
---
+++ src/repositories/products-repository.ts
@@ -0,0 +1,26 @@
+type ProductType = {
+    id: string
+    title: string,
+}
+
+let products:ProductType[] = [
+    {id: '1',title:"tomato"},
+    {id: '2',title:"banana"}
+
+]
+export const productsRepository = {
+    getProducts():ProductType[] {
+        return products
+    },
+    createProduct(title:string): ProductType | null {
+        if (!title.trim()) {
+            return null
+        }
+        const newProduct:ProductType = {
+            id:Date.now().toString(),
+            title:'New Product'
+        }
+        products.unshift(newProduct)
+        return newProduct
+    }
+}
