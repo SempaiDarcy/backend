7c9f941833139231a8843ca1d929771c6f87520d
SempaiDarcy <mr_darcy_1@mail.ru>
2025-09-26 18:13:26 +0400

    refactor(routers): вынесена валидация в middleware для blogs и posts
    - blogsRouter:
      - заменены ручные проверки на blogsValidation + idValidation
      - подключён inputValidationMiddleware для обработки ошибок
    - postsRouter:
      - вынесена валидация тела запроса в postsValidation
      - добавлена проверка id через idValidation
      - ошибки теперь обрабатываются централизованно через inputValidationMiddleware
    - удалена дублирующая логика валидации из самих роутов

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -1,6 +1,9 @@
 import {Request, Response, Router} from "express";
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {blogsRepository} from "../repositories/blogs-repository";
+import {idValidation} from "../middlewares/validation/id-validation";
+import {inputValidationMiddleware} from "../middlewares/validation/input-validation-middleware";
+import {blogsValidation} from "../middlewares/validation/blogs-validation";
 
 export const blogsRouter = Router({});
 
@@ -8,94 +11,59 @@ blogsRouter.get('/', (req: Request, res: Response) => {
     const blogs = blogsRepository.getBlogs()
     res.send(blogs)
 })
-blogsRouter.get('/:id', (req: Request, res: Response) => {
-    const blogId = req.params.id
-    const blog = blogsRepository.getBlocksById(blogId);
-    if (blog) {
-        res.status(200).send(blog)
-    } else res.sendStatus(404)
-})
+blogsRouter.get(
+    '/:id',
+    idValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const blogId = req.params.id
+        const blog = blogsRepository.getBlocksById(blogId);
+        if (blog) {
+            res.status(200).send(blog)
+        } else res.sendStatus(404)
+    })
 
-blogsRouter.post('/', authMiddleware, (req: Request, res: Response) => {
-    const {name, description, websiteUrl} = req.body;
-    const errorsMessages: { message: string; field: string }[] = [];
+blogsRouter.post(
+    '/',
+    authMiddleware,
+    blogsValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const {name, description, websiteUrl} = req.body;
+        const newBlog = blogsRepository.createBlog(name, description, websiteUrl)
+        res.status(201).send(newBlog)
+    })
+blogsRouter.put(
+    '/:id',
+    authMiddleware,
+    idValidation,
+    blogsValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const blogId = req.params.id;
 
-    if (!name?.trim()) {
-        errorsMessages.push({message: "Invalid name", field: "name"});
-    } else if (name?.trim().length > 15) {
-        errorsMessages.push({message: "name is too long", field: "name"});
-    }
+        const {name, description, websiteUrl} = req.body;
 
-    if (!description?.trim()) {
-        errorsMessages.push({message: "Invalid description", field: "description"});
-    } else if (description?.trim().length > 500) {
-        errorsMessages.push({message: "description is too long", field: "description"});
-    }
+        const isUpdated = blogsRepository.updateBlog(blogId, name, description, websiteUrl);
 
-    if (!websiteUrl?.trim()) {
-        errorsMessages.push({message: "Invalid websiteUrl", field: "websiteUrl"});
-    } else if (websiteUrl.trim().length > 100) {
-        errorsMessages.push({message: "websiteUrl is too long", field: "websiteUrl"});
-    } else {
-        const regex = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
-        if (!regex.test(websiteUrl.trim())) {
-            errorsMessages.push({message: "website url does not match the template", field: "websiteUrl"});
+        if (!isUpdated) {
+            return res.sendStatus(404)
         }
-    }
-    if (errorsMessages.length) {
-        return res.status(400).send({errorsMessages})
-    }
 
-    const newBlog = blogsRepository.createBlog(name, description, websiteUrl)
-    return res.status(201).send(newBlog)
-})
-blogsRouter.put('/:id', authMiddleware, (req: Request, res: Response) => {
-    const blogId = req.params.id;
-    const errorsMessages: { message: string; field: string }[] = [];
+        return res.sendStatus(204);
+    })
+blogsRouter.delete(
+    '/:id',
+    authMiddleware,
+    idValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const blogId = req.params.id;
 
-    const {name, description, websiteUrl} = req.body;
-
-    if (!name?.trim()) {
-        errorsMessages.push({message: "Invalid name", field: "name"});
-    } else if (name?.trim().length > 15) {
-        errorsMessages.push({message: "name is too long", field: "name"});
-    }
-
-    if (!description?.trim()) {
-        errorsMessages.push({message: "Invalid description", field: "description"});
-    } else if (description?.trim().length > 500) {
-        errorsMessages.push({message: "description is too long", field: "description"});
-    }
-
-    if (!websiteUrl?.trim()) {
-        errorsMessages.push({message: "Invalid websiteUrl", field: "websiteUrl"});
-    } else if (websiteUrl.trim().length > 100) {
-        errorsMessages.push({message: "websiteUrl is too long", field: "websiteUrl"});
-    } else {
-        const regex = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
-        if (!regex.test(websiteUrl.trim())) {
-            errorsMessages.push({message: "website url does not match the template", field: "websiteUrl"});
+        const isDeleted = blogsRepository.deleteBlog(blogId)
+        if (!isDeleted) {
+            return res.sendStatus(404);
         }
-    }
+        return res.sendStatus(204);
 
-    if (errorsMessages.length > 0) {
-        return res.status(400).send({errorsMessages})
-    }
-    const isUpdated = blogsRepository.updateBlog(blogId, name, description, websiteUrl);
-
-    if (!isUpdated) {
-        return res.sendStatus(404)
-    }
-
-    return res.sendStatus(204);
-})
-blogsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
-    const blogId = req.params.id;
-
-    const isDeleted = blogsRepository.deleteBlog(blogId)
-    if (!isDeleted) {
-        return res.sendStatus(404);
-    }
-    return res.sendStatus(204);
-
-})
+    })
--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,7 +1,10 @@
 import {Request, Response, Router} from "express";
-import {db} from "../db/db";
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {postsRepository} from "../repositories/posts-repository";
+import {postsValidation} from "../middlewares/validation/posts-validation";
+import {inputValidationMiddleware} from "../middlewares/validation/input-validation-middleware";
+import {blogsRepository} from "../repositories/blogs-repository";
+import {idValidation} from "../middlewares/validation/id-validation";
 
 export const postsRouter = Router({})
 
@@ -9,92 +12,68 @@ postsRouter.get('/', (req: Request, res: Response) => {
     const posts = postsRepository.getPosts()
     res.send(posts)
 })
-postsRouter.get('/:id', (req: Request, res: Response) => {
-    const postId = req.params.id;
-    const post = postsRepository.getPostsById(postId)
-    if (post) {
-        res.send(post)
-    } else res.sendStatus(404)
-})
-postsRouter.post('/', authMiddleware, (req: Request, res: Response) => {
-    const {title, shortDescription, content, blogId} = req.body;
-    const errorsMessages: { message: string, field: string }[] = []
+postsRouter.get(
+    '/:id',
+    idValidation,
+    (req: Request, res: Response) => {
+        const postId = req.params.id;
+        const post = postsRepository.getPostsById(postId)
+        if (post) {
+            res.send(post)
+        } else res.sendStatus(404)
+    })
+postsRouter.post(
+    '/',
+    authMiddleware,
+    postsValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const {title, shortDescription, content, blogId} = req.body;
 
-    if (!title?.trim()) {
-        errorsMessages.push({message: "Invalid title", field: "title"});
-    } else if (title?.trim().length > 30) {
-        errorsMessages.push({message: "title is too long", field: "title"});
-    }
+        const blog = blogsRepository.getBlocksById(blogId)!;
 
-    if (!shortDescription?.trim()) {
-        errorsMessages.push({message: "Invalid shortDescription", field: "shortDescription"});
-    } else if (shortDescription?.trim().length > 100) {
-        errorsMessages.push({message: "shortDescription is too long", field: "shortDescription"});
-    }
+        const newPost = postsRepository.createPosts(
+            title,
+            shortDescription,
+            content,
+            blogId,
+            blog!.name
+        )
+        res.status(201).send(newPost)
+    })
+postsRouter.put(
+    '/:id',
+    authMiddleware,
+    idValidation,
+    postsValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const postId = req.params.id;
+        const {title, shortDescription, content, blogId} = req.body;
 
-    if (!content?.trim()) {
-        errorsMessages.push({message: "Invalid content", field: "content"});
-    } else if (content?.trim().length > 1000) {
-        errorsMessages.push({message: "content is too long", field: "content"});
-    }
-    const blog = db.blogs.find(el => el.id === blogId)
-    if (!blog) {
-        errorsMessages.push({message: "Blog not found", field: "blogId"});
-    }
+        const isUpdated = postsRepository.updatePosts(
+            title,
+            shortDescription,
+            content,
+            blogId,
+            postId
+        )
 
-    if (errorsMessages.length) {
-        return res.status(400).send({errorsMessages})
-    }
-    const newPost = postsRepository.createPosts(
-        title,
-        shortDescription,
-        content,
-        blogId,
-        blog!.name
-    )
-    return res.status(201).send(newPost)
-})
-postsRouter.put('/:id', authMiddleware, (req: Request, res: Response) => {
-    const postId = req.params.id;
-    const errorsMessages: { message: string, field: string }[] = []
-    const {title, shortDescription, content, blogId} = req.body;
-
-    if (!title?.trim()) {
-        errorsMessages.push({message: "Invalid title", field: "title"});
-    } else if (title?.trim().length > 30) {
-        errorsMessages.push({message: "title is too long", field: "title"});
-    }
-
-    if (!shortDescription?.trim()) {
-        errorsMessages.push({message: "Invalid shortDescription", field: "shortDescription"});
-    } else if (shortDescription?.trim().length > 100) {
-        errorsMessages.push({message: "shortDescription is too long", field: "shortDescription"});
-    }
-
-    if (!content?.trim()) {
-        errorsMessages.push({message: "Invalid content", field: "content"});
-    } else if (content?.trim().length > 1000) {
-        errorsMessages.push({message: "content is too long", field: "content"});
-    }
-    const blog = db.blogs.find(el => el.id === blogId)
-    if (!blog) {
-        errorsMessages.push({message: "Blog not found", field: "blogId"});
-    }
-
-    if (errorsMessages.length) {
-        return res.status(400).send({errorsMessages})
-    }
-    const isUpdated = postsRepository.updatePosts(title,shortDescription,content,blogId,postId)
-    if (!isUpdated) {
-        return res.sendStatus(404);
-    }
-    return res.sendStatus(204);
-})
-postsRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
-    const postId = req.params.id;
-    const isDeleted = postsRepository.deletePosts(postId)
-    if (!isDeleted) {
-        return res.sendStatus(404)
-    }
-    return res.sendStatus(204)
-})
+        if (!isUpdated) {
+            return res.sendStatus(404);
+        }
+        return res.sendStatus(204);
+    })
+postsRouter.delete(
+    '/:id',
+    authMiddleware,
+    idValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+        const postId = req.params.id;
+        const isDeleted = postsRepository.deletePosts(postId)
+        if (!isDeleted) {
+            return res.sendStatus(404)
+        }
+        return res.sendStatus(204)
+    })
